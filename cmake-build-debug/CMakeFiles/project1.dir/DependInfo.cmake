a40b7f526149730bacf6a7473c2d9f743dc0ae02
XIANGYU CUI <136183369@qq.com>
2023-07-11 17:48:23 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/project1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/OneDrive/NEU/Khoury/Spring 2022/CS5150 Game Artificial Intelligence/project1/Project1/Project1/src/Project1.cpp" "CMakeFiles/project1.dir/src/Project1.cpp.obj" "gcc" "CMakeFiles/project1.dir/src/Project1.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/src/Project1.cpp.obj"
+  "CMakeFiles/project1.dir/src/Project1.cpp.obj.d"
+  "libproject1.dll.a"
+  "project1.exe"
+  "project1.exe.manifest"
+  "project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
